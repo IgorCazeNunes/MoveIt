b484b32d03c5001225d991c988e2158f5bc1bdc5
Crummorn <Igorcaze@gmail.com>
2021-02-23 15:07:36 -0300

    Chore: Refatoração do estilo da ExperienceBar.

--- src/components/ExperienceBar.tsx
+++ src/components/ExperienceBar.tsx
@@ -1,14 +1,16 @@
 import React from 'react';
 
+import styles from '../styles/components/ExperienceBar.module.css';
+
 export const ExperienceBar: React.FC = () => {
   return (
-    <header className="experience-bar">
+    <header className={styles.experienceBar}>
       <span>8 xp</span>
 
       <div>
         <div style={{ width: '50%' }} />
 
-        <span className="current-experience" style={{ left: '50%' }}>
+        <span className={styles.currentExperience} style={{ left: '50%' }}>
           300 xp
         </span>
       </div>
