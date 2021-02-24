be4dde836451ed68b98b9c96de884e0d85030e12
Crummorn <Igorcaze@gmail.com>
2021-02-24 14:17:25 -0300

    Feat: CompletedChallenges não é mais estatico.

--- src/components/CompletedChallenges.tsx
+++ src/components/CompletedChallenges.tsx
@@ -1,12 +1,15 @@
 import React from 'react';
+import { useChallenges } from '../hooks/ChallengesContext';
 
 import styles from '../styles/components/CompletedChallenges.module.css';
 
 export function CompletedChallenges(): JSX.Element {
+  const { challengesCompleted } = useChallenges();
+
   return (
     <div className={styles.completedChallengesContainer}>
       <span>Desafios completos</span>
-      <span>5</span>
+      <span>{challengesCompleted}</span>
     </div>
   );
 }
