155000b9b71c94596bcc667b1adfbc214ca5e92a
Crummorn <igorcaze@gmail.com>
2021-03-02 16:06:55 -0300

    Fix: Countdown agora começa em 25 minutos.

--- src/hooks/CountdownContext.tsx
+++ src/hooks/CountdownContext.tsx
@@ -32,7 +32,7 @@ export function CountdownProvider({
 }: CountdownProviderProps): JSX.Element {
   const { startNewChallenge } = useChallenges();
 
-  const [time, setTime] = useState(0.05 * 60);
+  const [time, setTime] = useState(25 * 60);
   const [isActive, setIsActive] = useState(false);
   const [hasFinished, setHasFinished] = useState(false);
 
@@ -46,7 +46,7 @@ export function CountdownProvider({
   const resetCountdown = useCallback(() => {
     clearTimeout(countdownTimeout);
     setIsActive(false);
-    setTime(0.05 * 60);
+    setTime(25 * 60);
     setHasFinished(false);
   }, []);
 
