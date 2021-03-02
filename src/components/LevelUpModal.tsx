9f2fddb9e33203b01a2a7bea23f2dae34ae17689
Crummorn <igorcaze@gmail.com>
2021-03-02 15:32:28 -0300

    Feat: Add Component LevelUpModal.

---
+++ src/components/LevelUpModal.tsx
@@ -0,0 +1,29 @@
+/* eslint-disable react/jsx-one-expression-per-line */
+import React from 'react';
+import { useChallenges } from '../hooks/ChallengesContext';
+
+import styles from '../styles/components/LevelUpModal.module.css';
+
+export function LevelUpModal(): JSX.Element {
+  const { level } = useChallenges();
+
+  return (
+    <div className={styles.overlay}>
+      <div className={styles.container}>
+        <header>{level}</header>
+
+        <strong>Parabéns</strong>
+        <p>Você alcançou um novo level.</p>
+
+        <button
+          type="button"
+          onClick={() => {
+            console.log('fechou');
+          }}
+        >
+          <img src="/icons/close.svg" alt="Fechar Modal" />
+        </button>
+      </div>
+    </div>
+  );
+}
